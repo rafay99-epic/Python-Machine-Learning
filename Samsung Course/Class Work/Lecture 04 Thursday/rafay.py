6b3da7352efcf44c735cd045f381202b041b1615
rafay99-epic <99marafay@gmail.com>
2021-10-01 10:38:44 +0500

    All Files

---
+++ Samsung Course/Class Work/Lecture 04 Thursday/rafay.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def cube(y):
+    return y*y*y
+
+
+# In[ ]:
+
+
+def evenOdd(x):
+    if (x % 2 == 0):
+        print("even")
+    else:
+        print("odd")
+
+def summ(a,b):
+    return a+b
+def mins(a,b):
+    return a-b
+def divide(a,b):
+    return a/b
+def product(a,b):
+    return a*b
